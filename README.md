0ce8ea0c90308ed5c97856c1cb1390dc3ebcbd48
elishakay <kramer1346@gmail.com>
2019-05-11 23:10:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Slack Chat App Clone using React
+
+Following tutorial from Udemy [link to tutorial](https://www.udemy.com/build-a-slack-chat-app-with-react-redux-and-firebase/)
---
+++ src/components/SidePanel/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import UserPanel from './UserPanel';
+import Channels from '../Channels';
+
+export default class SidePanel extends Component {
+
+  render() {
+    const { currentUser } = this.props;
+
+    console.log('currentUser var within the sidePanel: ',currentUser);
+
+    return (
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{
+          background: '#4c3c4c',
+          fontSize: '1.2rem'
+        }}
+      >
+      <UserPanel currentUser={currentUser} />
+      <Channels currentUser={currentUser} />
+      </Menu>
+    )
+  }
+}
